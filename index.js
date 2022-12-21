d56f1780e424db5fde76d60615e5564caaa73270
Ilya Mukasei <ilya.mukosey@gmail.com>
2022-12-21 18:28:35 +0300

    calculation

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const getNumbers = () => {
     let number1 = +prompt('Введите первое число:');
     let number2 = +prompt('Введите второе число:');
     let isNumber = typeof number1 === 'number' && typeof number2 === 'number';
-    let validNumber =number2 >= 2 && number2 <= 36;
-    if(isNumber && number1 && number2 && validNumber){
-        console.log(number1.toString(number2));
+    if(isNumber && number1 && number2){
+        console.log(`Ответ: ${number1 + number2}, ${number1 / number2}.`);
         return;
     }
     else{
